9223cb333515c051e0ea679552519826b8c4baf3
Dmitry Kovalenko <dmitry.kovalenko.mu@gmail.com>
2021-10-06 00:47:51 +0300

    add some components(header,footer, logo, movie-card, etc

---
+++ project/src/components/button-more/button-more.tsx
@@ -0,0 +1,9 @@
+function ButtonMore(): JSX.Element {
+  return (
+    <div className="catalog__more">
+      <button className="catalog__button" type="button">Show more</button>
+    </div>
+  );
+}
+
+export default ButtonMore;
